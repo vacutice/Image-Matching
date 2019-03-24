8554e427a44cc5186009860cd7017aab23094366
yongchun weng <2016301750020@whu.edu.cn>
2019-03-25 01:29:35 +0800

    Add files via upload

---
+++ ImageMatching_CorrelationCoefficient/ImageMatching_CorrelationCoefficient.cpp
@@ -0,0 +1,109 @@
+
+// ImageMatching_CorrelationCoefficient.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "ImageMatching_CorrelationCoefficient.h"
+#include "ImageMatching_CorrelationCoefficientDlg.h"
+#include<Windows.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CImageMatching_CorrelationCoefficientApp
+
+BEGIN_MESSAGE_MAP(CImageMatching_CorrelationCoefficientApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CImageMatching_CorrelationCoefficientApp construction
+
+CImageMatching_CorrelationCoefficientApp::CImageMatching_CorrelationCoefficientApp()
+{
+	// support Restart Manager
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
+
+	// TODO: add construction code here,
+	// Place all significant initialization in InitInstance
+}
+
+
+// The one and only CImageMatching_CorrelationCoefficientApp object
+
+CImageMatching_CorrelationCoefficientApp theApp;
+
+
+// CImageMatching_CorrelationCoefficientApp initialization
+
+BOOL CImageMatching_CorrelationCoefficientApp::InitInstance()
+{
+	TCHAR _szPath[MAX_PATH + 1] = { 0 };
+	GetModuleFileName(NULL, _szPath, MAX_PATH);
+	(_tcsrchr(_szPath, _T('\\')))[1] = 0;//删除文件名，只获得路径 字串
+	//_tcscpy_s(_szPath , L"E:\\match\\Debug\\bin");
+	_tcscat_s(_szPath, L"bin");
+	SetDllDirectory(_szPath);
+	// InitCommonControlsEx() is required on Windows XP if an application
+	// manifest specifies use of ComCtl32.dll version 6 or later to enable
+	// visual styles.  Otherwise, any window creation will fail.
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// Set this to include all the common control classes you want to use
+	// in your application.
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinApp::InitInstance();
+
+
+	AfxEnableControlContainer();
+
+	// Create the shell manager, in case the dialog contains
+	// any shell tree view or shell list view controls.
+	CShellManager *pShellManager = new CShellManager;
+
+	// Activate "Windows Native" visual manager for enabling themes in MFC controls
+	CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
+
+	// Standard initialization
+	// If you are not using these features and wish to reduce the size
+	// of your final executable, you should remove from the following
+	// the specific initialization routines you do not need
+	// Change the registry key under which our settings are stored
+	// TODO: You should modify this string to be something appropriate
+	// such as the name of your company or organization
+	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+
+	CImageMatching_CorrelationCoefficientDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with OK
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with Cancel
+	}
+	else if (nResponse == -1)
+	{
+		TRACE(traceAppMsg, 0, "Warning: dialog creation failed, so application is terminating unexpectedly.\n");
+		TRACE(traceAppMsg, 0, "Warning: if you are using MFC controls on the dialog, you cannot #define _AFX_NO_MFC_CONTROLS_IN_DIALOGS.\n");
+	}
+
+	// Delete the shell manager created above.
+	if (pShellManager != NULL)
+	{
+		delete pShellManager;
+	}
+
+	// Since the dialog has been closed, return FALSE so that we exit the
+	//  application, rather than start the application's message pump.
+	return FALSE;
+}
+
